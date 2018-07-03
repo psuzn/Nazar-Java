17505a2803ca3fd427e62d5c2b239e3367a3bd14
sujan poudel <spoudel347@gmail.com>
2018-07-03 11:26:20 +0545

    camera switch UI

--- app/src/main/java/com/sujanpoudel/nazar/Detection.java
+++ app/src/main/java/com/sujanpoudel/nazar/Detection.java
@@ -1,5 +1,6 @@
 package com.sujanpoudel.nazar;
 
+import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
@@ -8,14 +9,27 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.util.Log;
 import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class Detection extends CameraActivity {
+    private static final String sharedPrefenrences = "Settings";
+    View.OnClickListener cameraSwitch  = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(Detection.this,"Switch cmaera",Toast.LENGTH_SHORT).show();
+            Detection.this.recreate();
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         setContentView(R.layout.activity_detection);
         super.onCreate(savedInstanceState);
         Log.d("Nazar Debug","Detection activity");
+        ImageButton cameraSwitchButton = findViewById(R.id.cameraSwitch);
+        cameraSwitchButton.setOnClickListener(cameraSwitch);
     }
 
     @Override
