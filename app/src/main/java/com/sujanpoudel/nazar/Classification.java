423427e0b812f28a7e68f4844b31066a60253a63
Sunim Acharya <sunim.54@gmail.com>
2018-07-03 09:26:40 +0545

    Add dependencies

--- app/src/main/java/com/sujanpoudel/nazar/Classification.java
+++ app/src/main/java/com/sujanpoudel/nazar/Classification.java
@@ -8,8 +8,105 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.util.Log;
 import android.view.TextureView;
+import android.media.ImageReader.OnImageAvailableListener;
 
-public class Classification extends CameraActivity {
+import com.sujanpoudel.nazar.env.Logger;
+import com.sujanpoudel.nazar.env.BorderedText;
+import com.sujanpoudel.nazar.OverlayView.DrawCallback;
+import com.sujanpoudel.nazar.env.ImageUtils;
+
+public class Classification extends CameraActivity implements OnImageAvailableListener {
+
+    private static final Logger LOGGER = new Logger();
+    protected static final boolean SAVE_PREVIEW_BITMAP = false;
+
+    private ResultsView resultsView;
+
+    private Bitmap rgbFrameBitmap = null;
+    private Bitmap croppedBitmap = null;
+    private Bitmap cropCopyBitmap = null;
+
+    private long lastProcessingTimeMs;
+
+    private static final int INPUT_SIZE = 299;
+    private static final int IMAGE_MEAN = 128;
+    private static final float IMAGE_STD = 128;
+    private static final String INPUT_NAME = "Placeholder";
+    private static final String OUTPUT_NAME = "final_result";
+
+    private static final String MODEL_FILE = "file:///android_asset/retrained_graph.pb";
+    private static final String LABEL_FILE =
+        "file:///android_asset/retrained_labels.txt";
+
+    private static final boolean MAINTAIN_ASPECT = true;
+
+    private static final Size DESIRED_PREVIEW_SIZE = new Size(640, 480);
+
+
+    private Integer sensorOrientation;
+    private Classifier classifier;
+    private Matrix frameToCropTransform;
+    private Matrix cropToFrameTransform;
+
+    private BorderedText borderedText;
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.camera_connection_fragment;
+    }
+
+    @Override
+    protected Size getDesiredPreviewFrameSize() {
+        return DESIRED_PREVIEW_SIZE;
+    }
+
+    private static final float TEXT_SIZE_DIP = 10;
+
+    @Override
+    public void onPreviewSizeChosen(final Size size, final int rotation) {
+        final float textSizePx = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP, TEXT_SIZE_DIP, getResources().getDisplayMetrics());
+        borderedText = new BorderedText(textSizePx);
+        borderedText.setTypeface(Typeface.MONOSPACE);
+
+        classifier =
+            TensorFlowImageClassifier.create(
+                getAssets(),
+                MODEL_FILE,
+                LABEL_FILE,
+                INPUT_SIZE,
+                IMAGE_MEAN,
+                IMAGE_STD,
+                INPUT_NAME,
+                OUTPUT_NAME);
+
+        previewWidth = size.getWidth();
+        previewHeight = size.getHeight();
+
+        sensorOrientation = rotation - getScreenOrientation();
+        LOGGER.i("Camera orientation relative to screen canvas: %d", sensorOrientation);
+
+        LOGGER.i("Initializing at size %dx%d", previewWidth, previewHeight);
+        rgbFrameBitmap = Bitmap.createBitmap(previewWidth, previewHeight, Config.ARGB_8888);
+        croppedBitmap = Bitmap.createBitmap(INPUT_SIZE, INPUT_SIZE, Config.ARGB_8888);
+
+        frameToCropTransform = ImageUtils.getTransformationMatrix(
+            previewWidth, previewHeight,
+            INPUT_SIZE, INPUT_SIZE,
+            sensorOrientation, MAINTAIN_ASPECT);
+
+        cropToFrameTransform = new Matrix();
+        frameToCropTransform.invert(cropToFrameTransform);
+
+        addCallback(
+            new DrawCallback() {
+            @Override
+            public void drawCallback(final Canvas canvas) {
+                renderDebug(canvas);
+            }
+            });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_classification);
